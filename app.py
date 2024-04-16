ede555528564e3d9f6cd3b1ffda107ceff520320
simoposc <posch190076@sr.htlweiz.at>
2024-04-16 08:07:25 +0100

    working

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 CSV_FILE = 'votes.csv'
 
-# Initialisierung der Abstimmungsergebnisse aus der CSV-Datei, falls vorhanden
+
 votes = {}
 if os.path.exists(CSV_FILE) and os.path.getsize(CSV_FILE) > 0:
     with open(CSV_FILE, 'r') as file:
@@ -18,7 +18,6 @@ if os.path.exists(CSV_FILE) and os.path.getsize(CSV_FILE) > 0:
             else:
                 print("Invalid row in CSV:", row)
 else:
-    # Wenn die CSV-Datei nicht vorhanden ist oder leer ist, verwenden wir die initialen Abstimmungsergebnisse
     for i in range(1, 110):
         option_name = f"Option {i}"
         votes[option_name] = 0
@@ -37,24 +36,20 @@ def vote():
     option = request.form['option']
     num_votes_str = request.form['num_votes']
     
-    # Check if num_votes_str is not empty
+
     if num_votes_str.strip():
         try:
             num_votes = int(num_votes_str)
         except ValueError:
-            # Handle the case where num_votes_str is not a valid integer
             return "Invalid number of votes. Please enter a valid number."
         
-        # Aktualisierung der Abstimmungsergebnisse
         votes[option] += num_votes
-        
-        # Aktualisierung der CSV-Datei
+    
         with open(CSV_FILE, 'w') as file:
             writer = csv.writer(file)
             for option, count in votes.items():
                 writer.writerow([option, count])
     else:
-        # Handle the case where num_votes_str is empty
         return render_template('index.html', votes=votes)
     
     return redirect(url_for('index'))
@@ -75,12 +70,10 @@ def winner():
         elif key[0] == "A":
             females[key] = votes[key]
     
-    # Finde die Top-3-Optionen mit den meisten Stimmen
     top_males = sorted(males, key=males.get, reverse=True)[:5]
     top_females = sorted(females, key=females.get, reverse=True)[:5]
     return render_template('winner.html', top_males=top_males, top_females=top_females)
 
 
 if __name__ == '__main__':
-    # Aktualisiere die Abstimmungsergebnisse aus der CSV-Datei für Frauen
     app.run(host='0.0.0.0', port=5000)
